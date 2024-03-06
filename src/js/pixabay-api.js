9ea5a96945115ecae551183e0c4b8b8c943cb3f6
Svietlova <irasvet1997@gmail.com>
2024-03-06 10:39:18 +0200

    summary 1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,12 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const KEY = '42725994-1a6cd5432fa5183da0eabdd43';
+const BASE_URL = 'http://https://pixabay.com/api/';
+const QUERY = '';
+const IMAGE_TYPE = 'photo';
+const ORIENTAL = 'horizontal';
+const SAVESEARCH = true;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// import { functionName } from './pixabay-api.js';
+// import { renderFunction } from './render-functions.js';
