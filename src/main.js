65b2e5d52e2a6c84c799f7117874f6b307b501f5
Svietlova <irasvet1997@gmail.com>
2024-03-07 12:53:51 +0200

    fist finish

--- src/main.js
+++ src/main.js
@@ -1,4 +1,37 @@
-'use strict';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// import { functionName } from './pixabay-api.js';
-// import { renderFunction } from './render-functions.js';
+import { renderMarkup } from './js/render-functions.js';
+import { fetchImages } from './js/pixabay-api.js';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  nav: true,
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 150,
+});
+
+const form = document.querySelector('.search-form');
+const container = document.querySelector('.gallery');
+let searchWord = '';
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  container.innerHTML = '';
+  searchWord = form.elements.searchWord.value.trim();
+
+  fetchImages(searchWord)
+    .then(data => {
+      const markup = renderMarkup(data);
+      container.insertAdjacentHTML('beforeend', markup);
+
+      lightbox.refresh();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  form.reset();
+}
